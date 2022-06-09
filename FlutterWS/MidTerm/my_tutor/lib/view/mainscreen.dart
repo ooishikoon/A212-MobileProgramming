cef74fa177d13f6f26b1c79508e5ab1dd51a66f5
ooishikoon <skooi0307@hotmail.com>
2022-06-10 00:39:45 +0800

    update

--- FlutterWS/MidTerm/my_tutor/lib/view/mainscreen.dart
+++ FlutterWS/MidTerm/my_tutor/lib/view/mainscreen.dart
@@ -15,10 +15,18 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
+    late double screenHeight, screenWidth, resWidth;
+
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    if (screenWidth <= 600) {
+      resWidth = screenWidth;
+    } else {
+      resWidth = screenWidth * 0.75;
+    }
+
     return Scaffold(
-      body: const Center(
-        child: Text('Hello World'),
-      ),
+      body: const Center(),
       bottomNavigationBar: BottomAppBar(
           color: Colors.amber,
           child: SizedBox(
@@ -27,70 +35,122 @@ class _MainScreenState extends State<MainScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Expanded(
-                  flex: 1,
-                  child: SizedBox.fromSize(
-                    size: const Size(45, 45),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(Icons.menu_book),
-                        Text("Courses"),
-                      ],
-                    ),
-                  ),
-                ),
+                    flex: 1,
+                    child: SizedBox.fromSize(
+                      size: const Size(45, 45),
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.amber,
+                          child: InkWell(
+                            splashColor: Colors.amber,
+                            onTap: () {
+                              // Navigator.pushReplacement(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //         builder: (BuildContext context) =>
+                              //             const CourseScreen()));
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.menu_book),
+                                Text("Courses"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
                 Expanded(
-                  flex: 1,
-                  child: SizedBox.fromSize(
-                    size: const Size(45, 45),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(Icons.person),
-                        Text("Tutors"),
-                      ],
-                    ),
-                  ),
-                ),
+                    flex: 1,
+                    child: SizedBox.fromSize(
+                      size: const Size(45, 45),
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.amber,
+                          child: InkWell(
+                            splashColor: Colors.amber,
+                            onTap: () {
+                              // Navigator.pushReplacement(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //         builder: (BuildContext context) =>
+                              //             const CourseScreen()));
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.person),
+                                Text("Tutors"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
                 Expanded(
-                  flex: 1,
-                  child: SizedBox.fromSize(
-                    size: const Size(45, 45),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(Icons.subscriptions),
-                        Text("Subscribe"),
-                      ],
-                    ),
-                  ),
-                ),
+                    flex: 1,
+                    child: SizedBox.fromSize(
+                      size: const Size(45, 45),
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.amber,
+                          child: InkWell(
+                            splashColor: Colors.amber,
+                            onTap: () {},
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.subscriptions),
+                                Text("Subscribe"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
                 Expanded(
-                  flex: 1,
-                  child: SizedBox.fromSize(
-                    size: const Size(45, 45),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(Icons.favorite),
-                        Text("Favourite"),
-                      ],
-                    ),
-                  ),
-                ),
+                    flex: 1,
+                    child: SizedBox.fromSize(
+                      size: const Size(45, 45),
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.amber,
+                          child: InkWell(
+                            splashColor: Colors.amber,
+                            onTap: () {},
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.favorite),
+                                Text("Favourite"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
                 Expanded(
-                  flex: 1,
-                  child: SizedBox.fromSize(
-                    size: const Size(45, 45),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(Icons.category),
-                        Text("Profile"),
-                      ],
-                    ),
-                  ),
-                ),
+                    flex: 1,
+                    child: SizedBox.fromSize(
+                      size: const Size(45, 45),
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.amber,
+                          child: InkWell(
+                            splashColor: Colors.amber,
+                            onTap: () {},
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.category),
+                                Text("Profile"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
               ],
             ),
           )),
