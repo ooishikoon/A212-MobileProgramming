542165f906480be31b7f9073b769bb2e625f4705
ooishikoon <skooi0307@hotmail.com>
2022-06-09 21:18:00 +0800

    update

--- FlutterWS/MidTerm/my_tutor/lib/view/mainscreen.dart
+++ FlutterWS/MidTerm/my_tutor/lib/view/mainscreen.dart
@@ -16,26 +16,22 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('MY Tutor'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: const [
-            UserAccountsDrawerHeader(
-              accountName: Text("OSK"),
-              accountEmail: Text("skooi0307@hotmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN7aVEwAUVK0Uq7JTZqifhy8Q5zm1nC9LG3nyxwm4fAZAqCXMJr2qs5-hcsfxtppRbYcA&usqp=CAU"),
-              ),
-            ),
-          ],
-        ),
-      ),
       body: const Center(
         child: Text('Hello World'),
       ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.amber,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(icon: const Icon(Icons.menu_book), onPressed: () {}),
+            IconButton(icon: const Icon(Icons.person), onPressed: () {}),
+            IconButton(icon: const Icon(Icons.subscriptions),onPressed: () {}),
+            IconButton(icon: const Icon(Icons.favorite), onPressed: () {}),
+            IconButton(icon: const Icon(Icons.category), onPressed: () {}),
+          ],
+        ),
+      )
     );
   }
 }
