e35d47d42774d1277523afb8d81fe5010bde0825
ooishikoon <skooi0307@hotmail.com>
2022-06-10 01:49:52 +0800

    update

--- FlutterWS/MidTerm/my_tutor/lib/view/tutorsscreen.dart
+++ FlutterWS/MidTerm/my_tutor/lib/view/tutorsscreen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import 'package:my_tutor/constants.dart';
 
 class TutorsScreen extends StatefulWidget {
   const TutorsScreen({Key? key}) : super(key: key);
@@ -9,19 +14,164 @@ class TutorsScreen extends StatefulWidget {
 
 class _TutorsScreenState extends State<TutorsScreen> {
   @override
+  void initState() {
+    super.initState();
+    _loadTutors();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+    late double screenHeight, screenWidth, resWidth;
+
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    if (screenWidth <= 600) {
+      resWidth = screenWidth;
+    } else {
+      resWidth = screenWidth * 0.75;
+    }
+
+    return Scaffold(
+      body: const Center(),
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.amber,
+          child: SizedBox(
+            height: 55,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: SizedBox.fromSize(
+                      size: const Size(45, 45),
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.amber,
+                          child: InkWell(
+                            splashColor: Colors.amber,
+                            onTap: () {
+                              // Navigator.pushReplacement(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //         builder: (content) => MainScreen(
+                              //               user: user,
+                              //             )));
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.menu_book),
+                                Text("Courses"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: SizedBox.fromSize(
+                      size: const Size(45, 45),
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.amber,
+                          child: InkWell(
+                            splashColor: Colors.amber,
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (BuildContext context) =>
+                                          const TutorsScreen()));
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.person),
+                                Text("Tutors"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: SizedBox.fromSize(
+                      size: const Size(45, 45),
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.amber,
+                          child: InkWell(
+                            splashColor: Colors.amber,
+                            onTap: () {},
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.subscriptions),
+                                Text("Subscribe"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: SizedBox.fromSize(
+                      size: const Size(45, 45),
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.amber,
+                          child: InkWell(
+                            splashColor: Colors.amber,
+                            onTap: () {},
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.favorite),
+                                Text("Favourite"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: SizedBox.fromSize(
+                      size: const Size(45, 45),
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.amber,
+                          child: InkWell(
+                            splashColor: Colors.amber,
+                            onTap: () {},
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const <Widget>[
+                                Icon(Icons.category),
+                                Text("Profile"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
+              ],
+            ),
+          )),
     );
+  }
+
+  void _loadTutors() {
+    http.post(
+        Uri.parse(CONSTANTS.server + "/mytutor/mobile/php/load_tutors.php"),
+        body: {}).then((response) {
+      var data = jsonDecode(response.body);
+      if (response.statusCode == 200 && data['status'] == 'success') {
+        print(data);
+      } 
+    });
   }
 }
