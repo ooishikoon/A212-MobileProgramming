ca002da9af2ac464b8a2f8d7bcd22567286b8870
ooishikoon <skooi0307@hotmail.com>
2022-06-09 21:50:45 +0800

    update

--- FlutterWS/MidTerm/my_tutor/lib/view/mainscreen.dart
+++ FlutterWS/MidTerm/my_tutor/lib/view/mainscreen.dart
@@ -20,18 +20,80 @@ class _MainScreenState extends State<MainScreen> {
         child: Text('Hello World'),
       ),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.amber,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(icon: const Icon(Icons.menu_book), onPressed: () {}),
-            IconButton(icon: const Icon(Icons.person), onPressed: () {}),
-            IconButton(icon: const Icon(Icons.subscriptions),onPressed: () {}),
-            IconButton(icon: const Icon(Icons.favorite), onPressed: () {}),
-            IconButton(icon: const Icon(Icons.category), onPressed: () {}),
-          ],
-        ),
-      )
+          color: Colors.amber,
+          child: SizedBox(
+            height: 55,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: SizedBox.fromSize(
+                    size: const Size(45, 45),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Icon(Icons.menu_book),
+                        Text("Courses"),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: SizedBox.fromSize(
+                    size: const Size(45, 45),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Icon(Icons.person),
+                        Text("Tutors"),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: SizedBox.fromSize(
+                    size: const Size(45, 45),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Icon(Icons.subscriptions),
+                        Text("Subscribe"),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: SizedBox.fromSize(
+                    size: const Size(45, 45),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Icon(Icons.favorite),
+                        Text("Favourite"),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: SizedBox.fromSize(
+                    size: const Size(45, 45),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Icon(Icons.category),
+                        Text("Profile"),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
