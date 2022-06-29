4c190fe5ea953cf23e187d7e0a9dc46bf52918c7
ooishikoon <skooi0307@hotmail.com>
2022-06-29 15:17:12 +0800

    update

--- FlutterWS/Final/my_tutor/lib/view/profilescreen.dart
+++ FlutterWS/Final/my_tutor/lib/view/profilescreen.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:my_tutor/view/favouritescreen.dart';
 import 'package:my_tutor/view/mainscreen.dart';
 import 'package:my_tutor/view/subscribescreen.dart';
 import 'package:my_tutor/view/tutorsscreen.dart';
@@ -20,6 +21,16 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    // _nameController.text = widget.user.name.toString();
+    // _emailController.text = widget.user.email.toString();
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         bottomNavigationBar: BottomAppBar(
@@ -176,8 +187,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
               padding: const EdgeInsets.all(15),
               child: Stack(
                 children: [
-                  Align(
-                      alignment: Alignment.topRight, child: buildBookmark()),
+                  Align(alignment: Alignment.topRight, child: buildBookmark()),
+                  buildProfile(),
                 ],
               ),
             ),
@@ -199,5 +210,49 @@ class _ProfileScreenState extends State<ProfileScreen> {
           size: 40,
         ),
         onPressed: () {},
+      );
+
+  Widget buildProfile() => SizedBox(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () => {},
+                  child: SizedBox(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(200),
+                      child: Image.asset('assets/images/avatar.png'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+                flex: 6,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Name",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        "Email",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                )),
+          ],
+        ),
       );
 }
