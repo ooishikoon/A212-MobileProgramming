981dfaa1643a6509c65bda87666e86a0f59b4779
ooishikoon <skooi0307@hotmail.com>
2022-07-05 18:47:43 +0800

    update php

--- FlutterWS/Final/server/mytutor/mobile/php/dbconnect.php
+++ FlutterWS/Final/server/mytutor/mobile/php/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mytutor";
+$username = "moneymon_277290_mytutoruser";
+$password = "ZCf_g3sr=1ox";
+$dbname = "moneymon_277290_mytutor";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
