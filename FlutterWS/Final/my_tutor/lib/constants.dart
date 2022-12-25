d22b1bd5ca3b24b0ab26d73352d036cafb8065ee
ooishikoon <skooi0307@hotmail.com>
2022-12-25 21:41:46 +0800

    update

--- FlutterWS/Final/my_tutor/lib/constants.dart
+++ FlutterWS/Final/my_tutor/lib/constants.dart
@@ -1,5 +1,5 @@
 class CONSTANTS {
-  // static String server = "http://10.31.145.99"; // uum
+  static String server = "http://10.31.145.99"; // uum
   // static String server = "http://mytutor123.epizy.com"; //infinityfree
-  static String server = "http://moneymoney12345.com/277290";
+  // static String server = "http://moneymoney12345.com/277290";
 }
